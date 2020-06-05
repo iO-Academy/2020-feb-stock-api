21c3fcd8cfb25ff037c857470912f06ef8886f97
Alexander Reed <areed22496@gmail.com>
2020-06-05 16:47:55 +0100

    deleted extra sanitise in name validator

--- src/Validators/NameValidator.php
+++ src/Validators/NameValidator.php
@@ -4,8 +4,5 @@ namespace Portal\Validators;
 
 class NameValidator extends StringValidator
 {
-    public static function sanitiseString($validateData)
-    {
-        return trim(filter_var($validateData, FILTER_SANITIZE_STRING));
-    }
+
 }
