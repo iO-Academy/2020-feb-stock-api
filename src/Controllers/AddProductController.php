f9e95d961c3802ba49686aa24930e28b4a1fa810
Amanda Hardman <amandahrdmn@yahoo.com>
2020-06-05 16:48:13 +0100

    added middleware for dealing with json in post requests; took json data and used to instantiate new ProductEntity in the AddProductController

--- src/Controllers/AddProductController.php
+++ src/Controllers/AddProductController.php
@@ -5,6 +5,7 @@ namespace App\Controllers;
 
 
 use App\Abstracts\Controller;
+use App\Entities\ProductEntity;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
@@ -23,6 +24,14 @@ class AddProductController extends Controller
 
     public function __invoke(Request $request, Response $response, array $args)
     {
+        $newProductData = $request->getParsedBody();
+
+        $newProduct = new ProductEntity(
+            $newProductData['sku'],
+            $newProductData['name'],
+            $newProductData['price'],
+            $newProductData['stock']);
+
         return $response;
     }
 }
