95aa5a60aaa5dca448b51e9083cac641597f9c90
MaxG <mh.granger@yahoo.co.uk>
2020-06-05 16:55:51 +0100

    made addProduct function

--- src/Models/ProductModel.php
+++ src/Models/ProductModel.php
@@ -4,6 +4,7 @@
 namespace App\Models;
 
 
+use App\Interfaces\ProductEntityInterface;
 use App\Interfaces\ProductModelInterface;
 
 class ProductModel implements ProductModelInterface
@@ -17,5 +18,20 @@ class ProductModel implements ProductModelInterface
     public function __construct($db)
     {
         $this->db = $db;
+    }
+
+    public function addProduct(ProductEntityInterface $productEntity)
+    {
+        $array = ["sku"=>$productEntity->getSku(), "name"=>$productEntity->getName(),
+            "price"=>$productEntity->getPrice(), "stockLevel"=>$productEntity->getStockLevel()];
+
+        $query = $this->db->prepare("UPDATE `products`
+                                    SET `sku` = :sku
+                                        `name` = :name
+                                        `price` = :price
+                                        `stockLevel` = :stockLevel
+        ");
+        $result = $query->execute($array);
+        return $result;
     }
 }
