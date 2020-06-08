935d04815b4dcd7dc19ac6fafcb9670963773fcc
turnerror <christianturner@hotmail.co.uk>
2020-06-08 09:41:35 +0100

    Updated DB to fetch assoc and no longer static

--- src/Utilities/Database.php
+++ src/Utilities/Database.php
@@ -1,13 +1,14 @@
 <?php
 
-
 namespace App\Utilities;
 
 
 class Database
 {
-    static public function connect()
+    public function connect()
     {
-        return new \PDO('mysql:host=127.0.0.1;dbname=stock_api', 'root', 'password');
+        $db = new \PDO('mysql:host=127.0.0.1;dbname=stock_api', 'root', 'password');
+        $db->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
+        return $db;
     }
 }
