2f78cd3e0e3811ed7d7e87379c1170fea0ac1ce5
Alexander Reed <areed22496@gmail.com>
2020-06-08 14:41:58 +0100

    added get stock level controller

---
+++ src/Abstracts/Controllers/GetStockLevelController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Abstracts\Controller;
+use App\Entities\ProductEntity;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+class GetStockLevelController extends Controller
+{
+    private $productModel
+}
+
