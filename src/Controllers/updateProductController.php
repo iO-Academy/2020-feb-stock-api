4d88b0757fe0dba0c832e08e1c4e036219ff6dfa
Amanda Hardman <amandahrdmn@yahoo.com>
2020-06-08 12:05:37 +0100

    added handling of request json into productEntity and try-catch for if entity can't be instantiated

--- src/Controllers/updateProductController.php
+++ src/Controllers/updateProductController.php
@@ -4,7 +4,11 @@
 namespace App\Controllers;
 
 
-class updateProductController
+use App\Abstracts\Controller;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+class updateProductController extends Controller
 {
     private $productModel;
 
@@ -17,8 +21,25 @@ class updateProductController
         $this->productModel = $productModel;
     }
 
-    public function __invoke()
+    public function __invoke(Request $request, Response $response, array $args)
     {
-        
+        $productData = $request->getParsedBody()['product'];
+
+        try {
+            $product = new ProductEntity(
+                $productData['sku'],
+                $productData['name'],
+                $productData['price'],
+                $productData['stock']
+            );
+
+        } catch(\Throwable $e) {
+            $responseData = [
+                'success' => false,
+                'msg' => $e->getMessage(),
+                'data' => []
+            ];
+        }
     }
+
 }
