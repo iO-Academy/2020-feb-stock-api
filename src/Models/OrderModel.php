2221402040e6620c47c3b79f60ee49cd2c6357cc
Amanda Hardman <amandahrdmn@yahoo.com>
2020-06-09 16:51:05 +0100

    added queries to insert new order into db and add rows to linking table based on products in order

--- src/Models/OrderModel.php
+++ src/Models/OrderModel.php
@@ -16,4 +16,51 @@ class OrderModel implements OrderModelInterface
     {
         $this->db = $db;
     }
+
+    public function addOrder(OrderEntityInterface $orderEntity)
+    {
+        $order = [
+            'orderNumber' => $orderEntity->getOrderNumber(),
+            'customerEmail' => $orderEntity->getCustomerEmail(),
+            'shippingAddress1' => $orderEntity->getCustomerEmail(),
+            'shippingAddress2' => $orderEntity->getCustomerEmail(),
+            'shippingCity' => $orderEntity->getCustomerEmail(),
+            'shippingPostcode' => $orderEntity->getCustomerEmail(),
+            'shippingCountry' => $orderEntity->getCustomerEmail()
+        ];
+
+        $orderedProducts = $orderEntity->getOrderedProducts();
+
+        $this->db->beginTransaction();
+
+        $orderQuery = $this->db->prepare("INSERT INTO `orders`
+                                        (`orderNumber`, 
+                                        `customerEmail`, 
+                                        `shippingAddress1`, 
+                                        `shippingAddress2`,
+                                        `shippingCity`,
+                                        `shippingPostcode`,
+                                        `shippingCountry`,
+                                        )
+                                            VALUES (:orderNumber, 
+                                                    :customerEmail, 
+                                                    :shippingAddress1, 
+                                                    :shippingAddress2,
+                                                    :shippingCity,
+                                                    :shippingPostcode,
+                                                    :shippingCountry,)");
+
+        $orderQueryResult = $orderQuery->execute($order);
+
+        foreach($orderedProducts as $product) {
+            $sql[] = '("' . $order['orderNumber'] .'", ' . $product['sku'] . '", ' . $product['volumeNumber'] . ')';
+        }
+
+        $linkTableQuery = $this->db->prepare("INSERT INTO `orderedProducts`
+                                                  (`orderNumber`, `sku`, `volumeNumber`) 
+                                                  VALUES ". implode(",", $sql));
+
+        $this->db->commit();
+        return true;
+    }
 }
