75bafc86ce39d49f87884da6627bbcfa6ac71d02
turnerror <christianturner@hotmail.co.uk>
2020-06-10 16:56:00 +0100

    added unit tests for checkSufficientStock and doc comment

--- src/Validators/SufficientStockValidator.php
+++ src/Validators/SufficientStockValidator.php
@@ -4,10 +4,19 @@ namespace App\Validators;
 
 class SufficientStockValidator
 {
+    /**
+     * checks the following:
+     * - if products ordered SKU's are valid
+     * - that the volume ordered does not exceed the stock available for a product.
+     *
+     * @param array $orderedProducts
+     * @param array $productsStockLevels
+     * @throws \Exception thrown if checks above fail.
+     */
     public static function checkSufficientStock(array $orderedProducts, array $productsStockLevels): void
     {
         if (count($orderedProducts) !== count($productsStockLevels)){
-            throw new \Exception('Some SKUs provided for ordered products are invalid');
+            throw new \Exception('Some SKUs provided do not exist in DB');
         }
 
         for ($i = 0; $i < count($orderedProducts); $i++) {
@@ -15,7 +24,7 @@ class SufficientStockValidator
             $stockLevel = $productsStockLevels[$i]['stockLevel'];
 
             if ($volume > $stockLevel) {
-                throw new \Exception("Volume ordered for SKU: " . $orderedProducts[$i]['sku'] . " is higher than current stock");
+                throw new \Exception("Volume ordered for product with SKU: " . $orderedProducts[$i]['sku'] . " is higher than current stock");
             }
         }
 
