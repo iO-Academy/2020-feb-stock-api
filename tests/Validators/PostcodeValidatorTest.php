cba0d018a73bbae451065145c94b0ada6bcacc62
MaxG <mh.granger@yahoo.co.uk>
2020-06-10 14:16:12 +0100

    added PostcodeValidatorTest

---
+++ tests/Validators/PostcodeValidatorTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Validators;
+
+use App\Validators\PostcodeValidator;
+use Tests\TestCase;
+
+class PostcodeValidatorTest extends TestCase
+{
+    public function testValidatePostcodeSuccess()
+    {
+        $postcode = 'SM5 7UP';
+        $result = PostcodeValidator::validatePostcode($postcode);
+        $this->assertEquals('SM57UP', $result);
+    }
+
+    public function testValidatePostcodeFailure()
+    {
+        $postcode = 'AB12 CD34';
+        $this->expectExceptionMessage('Invalid postcode');
+        PostcodeValidator::validatePostcode($postcode);
+    }
+
+    public function testValidatePostcodeMalformed()
+    {
+        $postcode = ['AB2 8UK'];
+        $this->expectException(\TypeError::class);
+        PostcodeValidator::validatePostcode($postcode);
+    }
+}
