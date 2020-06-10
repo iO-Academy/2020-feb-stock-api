e9e4a7f650a52e677a4e7128bd7928b4f1a3712e
MaxG <mh.granger@yahoo.co.uk>
2020-06-10 14:11:35 +0100

    added tests to AddressValidatorTest

--- tests/Validators/AddressValidatorTest.php
+++ tests/Validators/AddressValidatorTest.php
@@ -2,9 +2,29 @@
 
 namespace Tests\Validators;
 
+use App\Validators\AddressValidator;
 use Tests\TestCase;
 
 class AddressValidatorTest extends TestCase
 {
+    public function testValidateAddressSuccess()
+    {
+        $address = '46 Old Mill Road';
+        $result = AddressValidator::validateAddress($address);
+        $this->assertEquals('46 Old Mill Road', $result);
+    }
 
+    public function testValidateAddressFailure()
+    {
+        $address = 'ABCDEF!';
+        $this->expectExceptionMessage('Invalid address');
+        AddressValidator::validateAddress($address);
+    }
+
+    public function testValidateAddressMalformed()
+    {
+        $address = [46, 'Old', 'Mill', 'Road'];
+        $this->expectException(\TypeError::class);
+        AddressValidator::validateAddress($address);
+    }
 }
