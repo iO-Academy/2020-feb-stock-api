c3505b22793a8031eda0661eaba1e32d81bc22fc
MaxG <mh.granger@yahoo.co.uk>
2020-06-10 16:38:50 +0100

    changed postcodevalidator regex to private const and created an addresstwovalidator

--- src/Validators/PostcodeValidator.php
+++ src/Validators/PostcodeValidator.php
@@ -4,6 +4,8 @@ namespace App\Validators;
 
 class PostcodeValidator
 {
+    private const POSTCODE_REGEX = '/([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\s?[0-9][A-Za-z]{2})/m';
+
     /**
      * Validates postcode
      * @param string $postcode
@@ -12,7 +14,7 @@ class PostcodeValidator
      */
     public static function validatePostcode(string $postcode)
     {
-        if(preg_match('/([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\s?[0-9][A-Za-z]{2})/m', $postcode)) {
+        if(preg_match(self::POSTCODE_REGEX, $postcode)) {
             return $postcode;
         } else {
             throw new \Exception('Invalid postcode');
